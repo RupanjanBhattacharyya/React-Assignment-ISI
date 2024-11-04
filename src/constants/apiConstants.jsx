568afa81392c061dfa942beb5f761f2ff220da34
Rupanjan Bhattacharyya <quest4knowledge1035@gmail.com>
2024-11-04 14:42:50 +0530

    server ff

--- src/constants/apiConstants.jsx
+++ src/constants/apiConstants.jsx
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://365b-103-240-96-107.ngrok-free.app';
+export const API_BASE_URL = 'https://5590-223-191-6-149.ngrok-free.app';
 export const ACCESS_TOKEN_NAME = 'login_access_token';
